69e3fb3e08782c9282281e5a921936a6786f8b91
Razanajatovo A. Toavina <razanajatovo.a.toavina@gmail.com>
2025-10-14 21:07:56 +0300

    Correction de la syntaxe et ajustement des commentaires dans le code du jeu

--- juste_prix.py
+++ juste_prix.py
@@ -1,10 +1,10 @@
 from random import randint
 import random
 
-# Version avec saisie du nombre à deviner par un joueur
+# # Version avec saisie du nombre à deviner par un joueur
 # nombre_a_deviner = int(input("Choisissez un nombre entre 1 et 100 : "))
 # while nombre_a_deviner < 1 or nombre_a_deviner > 100:
-#     nombre_a_deviner = int(input("Le nombre doit être entre 1 et 100. Choisissez un nombre entre 1 et 100 : "))
+#      nombre_a_deviner = int(input("Le nombre doit être entre 1 et 100. Choisissez un nombre entre 1 et 100 : "))
 
 # Version avec génération aléatoire du nombre à deviner
 nombre_a_deviner = random.randint(1, 100)
@@ -42,7 +42,7 @@ while 1:
     reponse = input().strip().lower()
     if reponse == "o":
         nombres_essais = 0
-        nombre_a_deviner = random.randint(1, 100)
+        nombre_a_deviner = random.randint(1, 100)}
     else:
         print(f"Vous avez gagné {nombre_parties_gagnées} parties.")
         print("Merci d'avoir joué !")
